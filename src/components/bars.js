f9654862f4131d152b65ab3456c9d22410827cb7
QPMulligan <quin.np1997@gmail.com>
2020-11-29 02:50:03 -0500

    See Previous

--- src/components/bars.js
+++ src/components/bars.js
@@ -12,11 +12,13 @@ function Bars(){
 
 //Creates an array of 20 values
 function Values(){
+    //creates array of 20 ints with values between 1-100
     var val = [];
     for  (var i = 0; i < 20; i++){
         val.push(Math.floor((Math.random() * 100) + 1));
     }
 
+    //Returns a display
     return(
         <div>
             {val.map((val, i) => (
